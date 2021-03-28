a7c8451b58ddef5d7e61bf8392b9d850e0742b75
AhMed SaLah <appsxapp@gmail.com>
2021-03-28 18:17:57 +0200

    Dashboard Component Initialized

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import Dashboard from './Dashboard';
 
 class App extends Component {
 	componentDidMount() {
 		this.props.dispatch(handleInitialData());
 	}
 	render() {
-		return <div>Initial Start</div>;
+		return <div>{this.props.loading ? null : <Dashboard />}</div>;
 	}
 }
 
-export default connect()(App);
+const mapStateToProps = ({ authUser }) => ({
+	loading: authUser === null
+});
+
+export default connect(mapStateToProps)(App);
